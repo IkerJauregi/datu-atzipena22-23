58738fa2d250131eba48183cff8bb8b68a53d026
Iker Jauregi <jauregi.iker@uni.eus>
2022-09-19 13:13:29 +0200

    Mayuskulak eginda

---
+++ dambi/src/main/java/iostreams/CopyCharactersMaiuskulaz.java
@@ -0,0 +1,32 @@
+package iostreams;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharactersMaiuskulaz {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                    if( c >= 97 && c <= 122){
+                        c = c -32;
+                    }
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
