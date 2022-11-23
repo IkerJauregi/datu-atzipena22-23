45a5e40f92137169fbe096ec9d72a259110ea8fe
Iker Jauregi <jauregi.iker@uni.eus>
2022-11-23 12:27:01 +0100

    2eba

---
+++ 2Eba/lehen-resapia/src/test/java/dambi/lehenresapia/LehenResapiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dambi.lehenresapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LehenResapiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- demo/src/main/java/mendienfitxategia/pojoak/Mendiak.java
+++
@@ -1,5 +0,0 @@
-package mendienfitxategia.pojoak;
-
-public class Mendiak {
-    
-}
